6953c3c0eec19422b25c3dca98488a355ef5fd41
Medgar Cynthea <medgarcy@oa-e6.msk.24-school.ru>
2024-03-31 21:41:17 +0300

    ex10

---
+++ src/ex10/day03_ex10.sql
@@ -0,0 +1,15 @@
+INSERT INTO person_order (id, person_id, menu_id, order_date)
+VALUES (
+    (SELECT MAX(id) FROM person_visits) + 1,
+    (SELECT id FROM person WHERE name = 'Denis'),
+    (SELECT id FROM menu WHERE pizza_name = 'Sicilian pizza'),
+    '2022-02-24'
+);
+
+INSERT INTO person_order (id, person_id, menu_id, order_date)
+VALUES (
+    (SELECT MAX(id) FROM person_visits) + 1,
+    (SELECT id FROM person WHERE name = 'Irina'),
+    (SELECT id FROM menu WHERE pizza_name = 'Sicilian pizza'),
+    '2022-02-24'
+);
